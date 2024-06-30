9cf43bafed19b88e2783accb1493549de46d64e5
Madhur <sharma15madhur@gmail.com>
2024-06-30 13:56:21 +0530

    added support for video

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,13 +2,33 @@ import React from 'react'
 
 function Main(props) {
   const {data} = props
+  console.log(data)
   return (
-    <div className='imgContainer'>
+    <div className="imgContainer">
+      {/* <img src={data.hdurl} alt={data.title || "bg-img"} className="bgImage" /> */}
 
-        <img src={data.hdurl} alt={data.title || 'bg-img'} 
-        className='bgImage'/>
+      <div className="media-container">
+        {data.media_type === "image" ? (
+          <img
+            src={data.hdurl}
+            alt={data.title || "bg-img"}
+            className="bgImage"
+          />
+        ) : (
+          <div className="video-wrapper">
+            <iframe
+              src={data.url}
+              title={data.title}
+              className="bgVideo"
+              frameBorder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Main
